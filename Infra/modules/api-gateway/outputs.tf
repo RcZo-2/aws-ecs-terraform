f991a45873358d187ab1ad155216154f3f84f4b7
RcZo-2 <sb3mk1999@gmail.com>
2025-10-03 15:48:14 +0800

    fix apigw

--- Infra/modules/api-gateway/outputs.tf
+++ Infra/modules/api-gateway/outputs.tf
@@ -5,17 +5,17 @@ output "target_group_arn" {
 
 output "api_gateway_id" {
   description = "The ID of the API Gateway"
-  value       = aws_apigatewayv2_api.main.id
+  value       = aws_apigatewayv2_api.http.id
 }
 
 output "alb_listener_arn" {
   description = "The ARN of the ALB listener"
-  value       = aws_lb_listener.http.arn
+  value       = aws_lb_listener.ecs.arn
 }
 
 output "vpc_link_id" {
   description = "The ID of the VPC link"
-  value       = aws_apigatewayv2_vpc_link.main.id
+  value       = aws_apigatewayv2_vpc_link.ecs.id
 }
 
 output "authorizer_id" {
